6c4db14fd6853d8071400e586e138b3b29319f7a
Joanne Lee <jokang1005@gmail.com>
2020-12-10 22:08:58 -0600

    gradebook frontend

--- gradebook_app_api/db/seeds.rb
+++ gradebook_app_api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+    StudentProfile.create(
+        student_id: Faker::Number.number(digits: 6),
+        name: Faker::Name.name,
+        address: Faker::Address.full_address,
+        guardian1: Faker::Name.name,
+        guardian1_number: Faker::PhoneNumber.cell_phone,
+        guardian2: Faker::Name.name,
+        guardian2_number: Faker::PhoneNumber.cell_phone
+    )
+end
+
+puts "Seeded database"
